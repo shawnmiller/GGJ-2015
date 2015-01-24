3703e40d27ed353f8ac446ca35e3f8d1b6549703
Shawn Miller <shawn.miller0@gmail.com>
2015-01-24 02:22:06 -0500

    added ColorSwapper and LevelColors

---
+++ Assets/Scripts/ColorSwapper.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorSwapper : MSingleton<ColorSwapper>
+{
+    private LevelColors levelColors;
+    private Color active;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    void SetActiveColor(string color)
+    {
+        int colorNum = ParseToNum(color);
+        if (levelColors && colorNum < levelColors.colors.Length)
+        {
+            active = levelColors.colors[colorNum];
+        }
+    }
+
+    private int ParseToNum(string color)
+    {
+        switch (color[0])
+        {
+            case '0':
+                return 0;
+            case '1':
+                return 1;
+            case '2':
+                return 2;
+            case '3':
+                return 3;
+            default:
+                Debug.Log("Invalid string on ColorSwapper.SetActiveColor: " + color);
+                return -1;
+        }
+    }
+
+    void OnLevelWasLoaded(int level)
+    {
+        levelColors = GameObject.FindObjectOfType<LevelColors>();
+    }
+}
---
+++ Assets/Scripts/LevelColors.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelColors : MonoBehaviour
+{
+    public Color[] colors = new Color[4];
+
+    void Start()
+    {
+        if (colors.Length > 4)
+        {
+            Debug.LogError("More than 4 colors allotted for this level.");
+            Debug.Break();
+        }
+
+        GameObject hud = GameObject.Find("HUD");
+        if (hud)
+        {
+            for (int i = 0; i < 4; ++i)
+            {
+                if (i < colors.Length)
+                {
+                    Image image = hud.transform.GetChild(i).GetComponent<Image>();
+                    image.enabled = true;
+                    image.color = colors[i];
+                }
+                else
+                {
+                    Image image = hud.transform.GetChild(i).GetComponent<Image>();
+                    image.enabled = false;
+                }
+            }
+        }
+    }
+}
