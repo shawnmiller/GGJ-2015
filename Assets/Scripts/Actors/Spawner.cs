5b2bcd787c0b664b39c1a049d2feb3ef72e4ce28
Matthew Martin <mmartin724@yahoo.com>
2015-01-24 18:48:57 -0500

    Clean up of spawner

--- Assets/Scripts/Actors/Spawner.cs
+++ Assets/Scripts/Actors/Spawner.cs
@@ -8,19 +8,6 @@ public class Spawner : MonoBehaviour
 	private float currentTime;
 
     public ProjectileInfo info;
-
-    //public Vector2 projectileDirection;
-    //public float projectileSpeed;
-    //public Vector3 projectileScale;
-    //public ColorType projectileColor;
-    //public PathType projectilePathType;
-    //[Tooltip("Center offset only works with non-linear paths.")]
-    //public float projectileCenterOffset;
-    //[Tooltip("Must be a value between 0 and 1")]
-    //public float projectileStartPoint;
-
-    //public bool permanant;
-    //public bool destroyOnCollide = true;
 	public bool preLoad = true;
 
 	void Start()
@@ -40,8 +27,6 @@ public class Spawner : MonoBehaviour
 		if (info.permanent)
 		{
 			spawnTime = 0f;
-            //projectileDirection = Vector2.zero;
-            //projectileSpeed = 0f;
             info.direction = Vector2.zero;
             info.speed = 0f;
 
@@ -53,6 +38,7 @@ public class Spawner : MonoBehaviour
 	{
 		if (!info.permanent)
 		{
+			if (spawnTime == 0f) { return; }
 			if (preLoad)
 			{
 				SpawnProjectile();
@@ -75,11 +61,5 @@ public class Spawner : MonoBehaviour
 		GameObject obj = ProjectileManager.Instance.Spawn(transform.position);
 		Projectile proj = obj.GetComponent<Projectile>();
         proj.Apply(info);
-        //proj.Velocity = projectileDirection;
-        //proj.MoveSpeed = projectileSpeed;
-        //proj.Scaler = projectileScale;
-        //proj.ColorType = projectileColor;
-        //proj.Permanent = permanant;
-        //proj.DestroyOnCollide = destroyOnCollide;
 	}
 }
