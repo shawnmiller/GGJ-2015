bc5239a8141bf477b3783a0ef9d1115b1c67506b
Shawn Miller <shawn.miller0@gmail.com>
2015-01-24 13:16:47 -0500

    added Extensions class

---
+++ Assets/Scripts/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public static class Extensions
+{
+    public static void AddUnique<T>(this List<T> list, T item)
+    {
+        if (!list.Contains(item))
+        {
+            list.Add(item);
+        }
+    }
+}
