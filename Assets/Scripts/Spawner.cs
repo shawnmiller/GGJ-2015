1e0637fbe72ed061c270ebf9f7988147c1043b26
Shawn Miller <shawn.miller0@gmail.com>
2015-01-23 23:45:48 -0500

    Spawner added

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject spawn;
+
+    public float spawnTime;
+    private float currentTime;
+
+    public float speed;
+
+	void Start ()
+    {
+        bool error = false;
+        if (!spawn)
+        {
+            error = true;
+            Debug.LogError("Spawner with a missing spawn object: " + name);
+        }
+        //if (!spawn.GetComponent<Projectile>())
+        //{
+        //    error = true;
+        //    Debug.LogError("Object to spawn is missing a Projectile script: " + spawn.name);
+        //}
+        if (error)
+        {
+            Debug.Break();
+        }
+	}
+	
+	void Update ()
+    {
+        currentTime += Time.deltaTime;
+        if (currentTime >= spawnTime)
+        {
+            currentTime -= spawnTime;
+            GameObject obj = Instantiate(spawn, transform.position, Quaternion.identity) as GameObject;
+            //Projectile projectile = obj.GetComponent<Projectile>();
+            //projectile.speed = speed;
+        }
+	}
+}
