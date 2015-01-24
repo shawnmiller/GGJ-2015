124b88d311cd72884be3b9718f92f84c8a9872aa
Shawn Miller <shawn.miller0@gmail.com>
2015-01-24 02:24:50 -0500

    fixed a derp

--- Assets/Scripts/ColorSwapper.cs
+++ Assets/Scripts/ColorSwapper.cs
@@ -11,30 +11,11 @@ public class ColorSwapper : MSingleton<ColorSwapper>
         DontDestroyOnLoad(this);
     }
 
-    void SetActiveColor(string color)
+    public void SetActiveColor(int color)
     {
-        int colorNum = ParseToNum(color);
-        if (levelColors && colorNum < levelColors.colors.Length)
+        if (levelColors && color < levelColors.colors.Length)
         {
-            active = levelColors.colors[colorNum];
-        }
-    }
-
-    private int ParseToNum(string color)
-    {
-        switch (color[0])
-        {
-            case '0':
-                return 0;
-            case '1':
-                return 1;
-            case '2':
-                return 2;
-            case '3':
-                return 3;
-            default:
-                Debug.Log("Invalid string on ColorSwapper.SetActiveColor: " + color);
-                return -1;
+            active = levelColors.colors[color];
         }
     }
 
