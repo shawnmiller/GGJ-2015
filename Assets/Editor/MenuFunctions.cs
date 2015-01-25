fb74d27b50d0c91ef45844c1cfd4c69e4e4b5a43
Shawn Miller <shawn.miller0@gmail.com>
2015-01-24 22:00:24 -0500

    editor script for usefulness

---
+++ Assets/Editor/MenuFunctions.cs
@@ -0,0 +1,38 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.IO;
+
+public static class MenuFunctions
+{
+    public static string lastLevel = "";
+
+    [MenuItem("GGJ/Play")]
+    public static void StartFromMenu()
+    {
+        lastLevel = EditorApplication.currentScene;
+        EditorApplication.OpenScene("Assets/Scenes/MainMenu.unity");
+        EditorApplication.isPlaying = true;
+    }
+
+    [MenuItem("GGJ/Return")]
+    public static void ReturnToLast()
+    {
+        if (lastLevel != "")
+        {
+            EditorApplication.isPlaying = false;
+            EditorApplication.OpenScene(lastLevel);
+        }
+    }
+
+    [MenuItem("GGJ/Edit Current")]
+    public static void EditCurrent()
+    {
+        string current = Application.loadedLevelName;
+        string path = Directory.GetFiles(Application.dataPath, current + ".unity", SearchOption.AllDirectories)[0];
+        Debug.Log(path);
+        path = path.Substring(path.IndexOf("Assets"));
+        Debug.Log(path);
+        EditorApplication.isPlaying = false;
+        EditorApplication.OpenScene(path);
+    }
+}
